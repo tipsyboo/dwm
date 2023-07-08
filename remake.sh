ff18a2be9d93710c4dc82d2130097dba5601a67e
tipsyboo <pavel.n87@gmail.com>
2023-07-08 10:37:11 +0300

    patch: always center floating window

--- remake.sh
+++ remake.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "### removing config.h ###" && rm -f config.h && echo "### compiling ###" && make && echo "installing" && sudo make clean install
+echo "### removing config.h ###" && rm -f config.h && echo "### compiling ###" && make && echo "### installing ###" && sudo make clean install
