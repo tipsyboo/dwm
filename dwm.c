2d5a5cfa8ff4535572e83d03e5cb09ecd87133d0
tipsyboo <pavel.n87@gmail.com>
2023-07-08 16:21:39 +0300

    disable window title

--- dwm.c
+++ dwm.c
@@ -740,10 +740,12 @@ drawbar(Monitor *m)
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
+			/* disable window title
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
+			*/
 		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
@@ -1251,8 +1253,10 @@ propertynotify(XEvent *e)
 		}
 		if (ev->atom == XA_WM_NAME || ev->atom == netatom[NetWMName]) {
 			updatetitle(c);
+			/* disable window title
 			if (c == c->mon->sel)
 				drawbar(c->mon);
+			*/
 		}
 		if (ev->atom == netatom[NetWMWindowType])
 			updatewindowtype(c);
